60e4a69152dc3c7ebf99a28d7396f2bdfa290ece
Jhonny-Q <vwkdms987@hanmail.net>
2025-07-11 07:31:56 +0900

    commit-2_+일자별

--- 명령어_Practice.py
+++ 명령어_Practice.py
@@ -1,3 +1,8 @@
 for x in range (1,10,1):
   print(x)
 
+# 7/04(금)Day2 if 문 : if ~ else, 중첩 if문 다중 if 문
+# 7/07(월)Day3 반복문 : for문, 중첩 for문, While 문
+# 7/08(화)Day4 기본 라이브러리_랜덤함수, List
+# 7/09(수)Day5 Tuple, Dictionary
+# 7/10(목)Day6 
